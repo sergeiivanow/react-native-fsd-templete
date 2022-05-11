4935e0233eebcd8acb1e1cc674855cd488afe736
sergeiivanow <38918636+sergeiivanow@users.noreply.github.com>
2022-05-12 01:51:25 +0300

    Add redux toolkit

--- template/index.js
+++ template/index.js
@@ -1,11 +1,27 @@
 /**
  * @format
  */
-
+import React from 'react';
 import {Navigation} from 'react-native-navigation';
 import App from './App';
+import {configureStore} from '@reduxjs/toolkit';
+import {Provider} from 'react-redux';
 
-Navigation.registerComponent('com.myApp.WelcomeScreen', () => App);
+const store = configureStore({
+  reducer: {},
+});
+
+Navigation.registerComponent(
+  'com.myApp.WelcomeScreen',
+  () => props =>
+    (
+      <Provider store={store}>
+        <App {...props} />
+      </Provider>
+    ),
+  () => App,
+);
+
 Navigation.events().registerAppLaunchedListener(() => {
   Navigation.setRoot({
     root: {
